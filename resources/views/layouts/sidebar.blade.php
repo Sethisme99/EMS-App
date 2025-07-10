3e8d3b570d2fd3b3788630b95fb87da3a5285ca1
Sethisme99 <raseth674@gmail.com>
2025-07-09 23:38:27 -0500

    Initial commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,14 @@
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex flex-column flex-shrink-0 p-3">
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link link-body-emphasis">
+                        <i class="fas fa-dashboard"></i>
+                        Dashboard
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
